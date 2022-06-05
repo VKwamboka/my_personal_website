875ff5cef4a6953228f14ed701c980e702ebfcb5
verah <verahnyagaka101@gmail.com>
2022-06-05 23:44:16 +0300

    phase-0

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+// const h2 = document.createElement("h2");
+// h2.textContent = "This content added by JavaScript";
+// document.querySelector("body").appendChild(h2);
+window.onload = function () {
+    document.body.className = '';
+}
+window.ontouchmove = function () {
+    return false;
+}
+window.onorientationchange = function () {
+    document.body.scrollTop = 0;
+}
+window.addEventListener("load", function() {
+    alert("Welcome!");
+})
+
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "block") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
